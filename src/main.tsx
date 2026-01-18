8338d6ff4f75f603ff9d19608e5c7c71ccbc794b
Ruslan Ivanov <rusivary@gmail.com>
2026-01-19 01:58:33 +0400

    Added 404 not found page

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./style.css";
 import HomePage from "./pages/HomePage";
 import SolarSystemPage from "./pages/SolarSystemPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const root = document.getElementById("root");
 
@@ -17,6 +18,7 @@ ReactDOM.createRoot(root).render(
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/solar-system" element={<SolarSystemPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import Lead from "../../components/atoms/Lead";
+import "../../components/atoms/Button/Button.css";
+import "./NotFoundPage.css";
+
+const NotFoundPage = () => {
+  return (
+    <main className="not-found">
+      <div className="not-found__content">
+        <span className="not-found__code">404</span>
+        <h1 className="not-found__title">Page not found</h1>
+        <Lead size="large" className="not-found__lead">
+          The route you requested does not exist. Try returning to the main scene.
+        </Lead>
+        <div className="not-found__actions">
+          <Link className="button button--primary" to="/">
+            Back to home
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default NotFoundPage;
